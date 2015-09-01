95b8050ea7c373ff2fad60b2ad6ae52780746802
Sebastian Bensusan <sbensu@gmail.com>
2015-09-01 17:15:47 +0200

    WIP: cleanup

--- src/deps/tree.js
+++ src/deps/tree.js
@@ -5,7 +5,7 @@ goog.require('cljsjs.d3');
 // http://bl.ocks.org/mbostock/1138500
 
 deps.tree.config = {w: 1600, h: 1200,
-                    rx: 60, ry: 20,
+                    rx: 60, ry: 30,
                     fill: d3.scale.category20()};
 
 deps.tree.force = d3.layout.force()
@@ -74,54 +74,4 @@ deps.tree.drawTree = function(nodeId, json) {
       .scale(initialScale)
       .event(root);
   root.attr("height", g.graph().height * initialScale + 40);
-  // var link = root.selectAll("line")
-  //     .data(json.edges)
-  //     .enter().append("svg:line");
-
-  // var node = root.selectAll("g").data(json.nodes);
-
-  // var box = node.enter().append("g");
-
-  // var circle = box.append("svg:ellipse")
-  //     .attr("rx", deps.tree.config.rx - .75)
-  //     .attr("ry", deps.tree.config.ry)
-  //     .style("fill", function(d) { 
-  //         var groupName = d.name.split(".")[0];
-  //         return deps.tree.config.fill(groupName);
-  //     })
-  //     .style("stroke", function(d) {
-  //         return d3.rgb(deps.tree.config.fill(d.group)).darker();
-  //     })
-  //     .call(deps.tree.force.drag);
-
-  // var label = box.append("text")
-  //       .text(function(d){
-  // 	    return d.name;
-  //       })
-  //   	.attr("text-anchor", "middle");
-  // deps.tree.force
-  //     .nodes(json.nodes)
-  //     .links(json.edges)
-  //     .on("tick", tick)
-  //     .start();
-
-  function tick(e) {
-
-    // Push sources up and targets down to form a weak deps.tree.
-    var k = 6 * e.alpha;
-    json.edges.forEach(function(d, i) {
-      d.source.y -= k;
-      d.target.y += k;
-    });
-
-  //   node.attr("transform", function(d) {
-  //       return "translate(" + d.x + "," + d.y + ")";
-  //   })
-  //       .attr("y", function(d) { return d.y; });
-
-  //   link.attr("x1", function(d) { return d.source.x; })
-  //       .attr("y1", function(d) { return d.source.y; })
-  //       .attr("x2", function(d) { return d.target.x; })
-  //       .attr("y2", function(d) { return d.target.y; });
-  }
 };
